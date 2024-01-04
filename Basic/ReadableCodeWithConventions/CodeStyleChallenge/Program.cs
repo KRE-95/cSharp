a96971b621f2bbeb5593131c8d043ce69551326f
KRE-95 <Kainat.elahipad@gmail.com>
2024-01-04 14:59:57 +0100

    Applied higher-level comment quality

--- Basic/ReadableCodeWithConventions/CodeStyleChallenge/Program.cs
+++ Basic/ReadableCodeWithConventions/CodeStyleChallenge/Program.cs
@@ -1,14 +1,12 @@
-﻿// given code 
-string originalMessage = "The quick brown fox jumps over the lazy dog."; // note variable name has to be changed
-// convert the message into a char array
-char[] message = str.ToCharArray();  // same here 
-// Reverse the chars
+﻿/* This code reverse a message, counts the number of times
+a particular character appears, then prints the results to the console  */
+
+// given code 
+string originalMessage = "The quick brown fox jumps over the lazy dog."; 
+char[] message = str.ToCharArray();   
 Array.Reverse(charMessage);
 int x = 0;
-// count the o's
 foreach (char i in message) { if (i == 'o') { x++; } }
-// convert it back to a string
-string new_Message = new String(message); // same here variable name
-// print it out
+string new_Message = new String(message); 
 Console.WriteLine(new_Message);
 Console.WriteLine($"'o' appears {x} times.");
