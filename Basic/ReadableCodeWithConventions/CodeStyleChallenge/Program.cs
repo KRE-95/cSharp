ed8db9794a652c5ce92e642906a11a56e7259948
KRE-95 <Kainat.elahipad@gmail.com>
2024-01-04 15:05:49 +0100

    Applied whitespace rules

--- Basic/ReadableCodeWithConventions/CodeStyleChallenge/Program.cs
+++ Basic/ReadableCodeWithConventions/CodeStyleChallenge/Program.cs
@@ -1,12 +1,21 @@
 ﻿/* This code reverse a message, counts the number of times
 a particular character appears, then prints the results to the console  */
-
-// given code 
 string originalMessage = "The quick brown fox jumps over the lazy dog."; 
+
 char[] message = str.ToCharArray();   
 Array.Reverse(charMessage);
+
 int x = 0;
-foreach (char i in message) { if (i == 'o') { x++; } }
+
+foreach (char i in message) 
+{ 
+    if(i == 'o') 
+    {
+         x++; 
+    } 
+ }
+
 string new_Message = new String(message); 
+
 Console.WriteLine(new_Message);
 Console.WriteLine($"'o' appears {x} times.");
