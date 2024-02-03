c3e94917abf60eb5d9039d0842cd17c56f913f50
KRE-95 <Kainat.elahipad@gmail.com>
2024-02-03 21:53:42 +0100

    Implement user access control based on business rules
    - Define business rules for user access in a high-level block comment.
    - Use if-else statements to evaluate complex conditions.
    - Check user roles and career level using the Contains() method on the permission string.
    - Display appropriate output messages based on the evaluated conditions.
    - Include detailed comments for clarity and understanding.
    - Update variable names and refine code structure for improved readability.

--- Basic/Intermediate/ConditionalOperator/AccessLogicController/Program.cs
+++ Basic/Intermediate/ConditionalOperator/AccessLogicController/Program.cs
@@ -1,3 +1,28 @@
 ﻿// variable
 string permission = "Admin|Manager";
 int level = 55;
+
+// The business rules is complex - use if-else statement
+
+string outputMessage;
+if (permission.Contains("Admin") && level > 55) 
+{
+    outputMessage = "Welcome, Super Admin user. ";
+}
+else if (permission.Contains("Admin") && level <= 55) 
+{
+    outputMessage = "Welcome, Admin user";
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+     outputMessage = "Contact an Admin for access.";
+}
+else if (permission.Contains("Manager") && level < 20) 
+{
+    outputMessage = "You do not have sufficient privileges.";
+}
+else {
+    outputMessage = "You do not have sufficient privileges.";
+}
+
+Console.WriteLine(outputMessage);
